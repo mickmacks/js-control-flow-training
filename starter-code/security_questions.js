696a759306a379bb9fce3838413a585e686c6bd5
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-03-28 22:58:16 -0700

    I did it! Sorry for not committing more :(

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,16 @@
 console.log("security_questions.js is loaded");
+
+var securityQuestions = {
+	questionOne : {question: 'What is your favorite food', expectedAnswer: 'Ramen'}, 
+	questionTwo : {question: 'What is your mother\'s maiden name?', expectedAnswer: 'Smith'}, 
+	questionThree : {question: 'What street did you grow up on', expectedAnswer: 'Nightingale'}
+};
+
+for(var key in securityQuestions) {
+    var answer = prompt(securityQuestions[key].question, 'Please enter your answer');
+    console.log(answer);
+    if (answer != securityQuestions[key].expectedAnswer) {
+      alert('You have failed');
+      break;
+    }
+}
